9ace9e82deb389b691661e7960bd55e6ae85fa2e
Nilrudra1999 <nilrudram@gmail.com>
2025-03-28 03:44:29 -0400

    a6- db connection overhead correction

--- user-service.js
+++ user-service.js
@@ -23,34 +23,21 @@ let userSchema = new Schema({
 });
 let User;
 
-module.exports.connect = function () {
-    return new Promise(function (resolve, reject) {
-        let db = mongoose.createConnection(mongoDBConnectionString);
-
-        db.on('error', err => {
-            reject(err);
-        });
-
-        db.once('open', () => {
-            User = db.model("users", userSchema);
-            resolve();
-        });
-    });
-};
+mongoose.connect(mongoDBConnectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => {
+        console.error('Unable to connect to MongoDB:', err);
+        process.exit(1);
+});
 
 module.exports.registerUser = function (userData) {
     return new Promise(function (resolve, reject) {
-
         if (userData.password != userData.password2) {
             reject("Passwords do not match");
         } else {
-
             bcrypt.hash(userData.password, 10).then(hash => {
-
                 userData.password = hash;
-
                 let newUser = new User(userData);
-
                 newUser.save().then(() => {
                     resolve("User " + userData.userName + " successfully registered");  
                 }).catch(err => {
@@ -67,7 +54,6 @@ module.exports.registerUser = function (userData) {
 
 module.exports.checkUser = function (userData) {
     return new Promise(function (resolve, reject) {
-
         User.findOne({ userName: userData.userName })
             .exec()
             .then(user => {
@@ -86,7 +72,6 @@ module.exports.checkUser = function (userData) {
 
 module.exports.getFavourites = function (id) {
     return new Promise(function (resolve, reject) {
-
         User.findById(id)
             .exec()
             .then(user => {
@@ -98,9 +83,7 @@ module.exports.getFavourites = function (id) {
 };
 
 module.exports.addFavourite = function (id, favId) {
-
     return new Promise(function (resolve, reject) {
-
         User.findById(id).exec().then(user => {
             if (user.favourites.length < 50) {
                 User.findByIdAndUpdate(id,
@@ -133,7 +116,6 @@ module.exports.removeFavourite = function (id, favId) {
 
 module.exports.getHistory = function (id) {
     return new Promise(function (resolve, reject) {
-
         User.findById(id)
             .exec()
             .then(user => {
@@ -145,9 +127,7 @@ module.exports.getHistory = function (id) {
 };
 
 module.exports.addHistory = function (id, historyId) {
-
     return new Promise(function (resolve, reject) {
-
         User.findById(id).exec().then(user => {
             if (user.favourites.length < 50) {
                 User.findByIdAndUpdate(id,
